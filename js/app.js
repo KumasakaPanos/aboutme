a19e5a36e54c6a4ad4664f8b194bb00048a5dd3f
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-02-08 12:53:21 -0800

    Updated with eslintrc compliance and fun function functionality

--- js/app.js
+++ js/app.js
@@ -1,130 +1,128 @@
-"use strict";
-var correctAnswers=0
-var username=prompt("Hello user, what's ya name?")
-var ansOneInitial=prompt("Am I a minor?");
-var ansOne=ansOneInitial.toLowerCase();
-if(ansOne === "yes"||ansOne === "y") {
-alert("No... Do I really look that young?")
-}
-else if(ansOne === "no"||ansOne === "n") {
-    alert("Right.")
-    correctAnswers+=1
-    console.log("User replied "+ansOne)
-}
-else
-    alert("Yes or No only please.")
-   console.log("For question one user replied "+ansOne)
-var ansTwoInitial=prompt("Is my Laptop older than Windows 10?");
-var ansTwo=ansTwoInitial.toLowerCase();
-if(ansTwo === "yes"||ansTwo === "y") {
-    alert("Right. I still like it though...")
-    correctAnswers+=1
-}
-else if(ansTwo === "no"||ansTwo === "n") {
-    alert("No, it is, but I stil like it.")
-}
-else {
-alert("Yes or No only please.")
-}
-console.log("For question two user replied "+ansTwo)
-var ansThreeInitial=prompt("Do I use earbuds?");
-var ansThree=ansThreeInitial.toLowerCase();
-if(ansThree === "yes"||ansThree === "y") {
-    alert("No, I hate putting them in.");
-}
-else if(ansThree === "no"||ansThree === "n") {
-alert("Right!");
-correctAnswers+=1
-}
-else {
-alert("Yes or No only please.");
-}
-console.log("For question three user replied "+ansThree)
-var ansFourInitial=prompt("Are all dogs beautiful?");
-var ansFour=ansFourInitial.toLowerCase();
-if(ansFour === "y"||ansFour === "yes") {
-            alert("Indeed.");
-            correctAnswers+=1
-        }
-        else if(ansFour === "no"||ansFour === "n") {
-            alert("How dare you");
-        }
-        else {
-        alert("Yes or No only please.")
-        }
-        console.log("For question four user replied "+ansFour)
-var ansFiveInitial=prompt("Is my backpack Black?");
-var ansFive=ansFiveInitial.toLowerCase();
-if(ansFive === "yes"||ansFive === "y") {
-    alert("True.")
-    correctAnswers+=1
-}
-else if(ansFive === "no"||ansFive === "n") {
-    alert("Maybe I got a new one... Did I break it?")
-}
-else {
-alert("Yes or No only please.")
-}
-console.log("For question five user replied "+ansFive)
-var correct=false
-var attempt=0
-var answerNum=prompt("What is the answer?")
-do{
-if(answerNum=="42")
-{
-    correct=true
-}
-else
-{
-    attempt+=1
-    if(answerNum>42)
-    {
-      alert("Too high.")
+'use strict';
+var correctAnswers = 0;
+var username = prompt('Hello user, what\'s ya name?');
+function question6() {
+  var correct = false;
+  var attempt = 0;
+  var answer = prompt('What is the answer?');
+  do {
+    if (answer=== '42') {
+      correct = true;
     }
-    if(answerNum<42)
-    {
-      alert("Too low.")
+    else {
+      attempt += 1;
+      if (answer > 42) {
+        alert('Too high.');
+      }
+      if (answer < 42) {
+        alert('Too low.');
+      }
+      alert(4 - attempt + ' tries left.');
+      answer = prompt('Try again.');
     }
-    alert(4-attempt+" tries left.")
-    answerNum=prompt("Try again.")
+  } while (attempt < 3 && correct === false);
+  if (answer === '42') {
+    alert('Good number. But what\'s the question?');
+    correctAnswers+=1;
+  }
+  else {
+    alert('Hey, you\'ll get it next time.');
+  }
+  console.log('The user finished with the question ' + correct + ' after ' + attempt + ' tries.');
 }
-}while(attempt<3&&correct===false)
-if(answerNum=="42")
-{
-alert("Good number. But what's the question?")
-correctAnswers+=1
+function question7() {
+  var correctState = false;
+  var attempt = 0;
+  var answer = prompt('Name a state or Province I have vacationed in.');
+  answer = answer.toLowerCase();
+  do {
+    if (answer === 'oregon' || answer === 'hawaii' || answer === 'washington' || answer === 'victoria') {
+      correctState = true;
+    }
+    else {
+      alert(4 - attempt + ' tries left.');
+      answer = prompt('Try again.');
+      attempt += 1;
+    }
+  } while (attempt < 4 && correctState === false);
+  if (correctState === true) {
+    alert('Good guess!');
+    correctAnswers += 1;
+  }
+  else {
+    alert('Hey, you\'ll get it next time.');
+  }
+  console.log('The user finished with the question ' + correctState + ' after ' + attempt + ' tries.');
 }
-else
-{
-alert("Hey, you'll get it next time.")
+function questionsYesNo() {
+  var ansOneInitial = prompt('Am I a minor?');
+  var ansOne = ansOneInitial.toLowerCase();
+  if (ansOne === 'yes' || ansOne === 'y') {
+    alert('No... Do I really look that young?');
+  }
+  else if (ansOne === 'no' || ansOne === 'n') {
+    alert('Right.');
+    correctAnswers += 1;
+    console.log('User replied ' + ansOne);
+  }
+  else
+    alert('Yes or No only please.');
+  console.log('For question one user replied ' + ansOne);
+  var ansTwoInitial = prompt('Is my Laptop older than Windows 10?');
+  var ansTwo = ansTwoInitial.toLowerCase();
+  if (ansTwo === 'yes' || ansTwo === 'y') {
+    alert('Right. I still like it though...');
+    correctAnswers += 1;
+  }
+  else if (ansTwo === 'no' || ansTwo === 'n') {
+    alert('No, it is, but I stil like it.');
+  }
+  else {
+    alert('Yes or No only please.');
+  }
+  console.log('For question two user replied ' + ansTwo);
+  var ansThreeInitial = prompt('Do I use earbuds?');
+  var ansThree = ansThreeInitial.toLowerCase();
+  if (ansThree === 'yes' || ansThree === 'y') {
+    alert('No, I hate putting them in.');
+  }
+  else if (ansThree === 'no' || ansThree === 'n') {
+    alert('Right!');
+    correctAnswers += 1;
+  }
+  else {
+    alert('Yes or No only please.');
+  }
+  console.log('For question three user replied ' + ansThree);
+  var ansFourInitial = prompt('Are all dogs beautiful?');
+  var ansFour = ansFourInitial.toLowerCase();
+  if (ansFour === 'y' || ansFour === 'yes') {
+    alert('Indeed.');
+    correctAnswers += 1;
+  }
+  else if (ansFour === 'no' || ansFour === 'n') {
+    alert('How dare you');
+  }
+  else {
+    alert('Yes or No only please.');
+  }
+  console.log('For question four user replied ' + ansFour);
+  var ansFiveInitial = prompt('Is my backpack Black?');
+  var ansFive = ansFiveInitial.toLowerCase();
+  if (ansFive === 'yes' || ansFive === 'y') {
+    alert('True.');
+    correctAnswers += 1;
+  }
+  else if (ansFive === 'no' || ansFive === 'n') {
+    alert('Maybe I got a new one... Did I break it?');
+  }
+  else {
+    alert('Yes or No only please.');
+  }
+  console.log('For question five user replied ' + ansFive);
 }
-var triesNumber=attempt+1
-console.log("The user finished with the question "+correct+" after "+attempt+" tries.")
-var correctState=false
-var attemptState=0
-var answerState=prompt("Name a state or Province I have vacationed in.")
-answerState=answerState.toLowerCase()
-do{
-    if(answerState==="oregon"||answerState==="hawaii"||answerState==="washington"||answerState==="victoria")
-    {
-        correctState=true
-    }
-    else
-    {
-        alert(4-attemptState+" tries left.")
-        answerState=prompt("Try again.")
-        attemptState+=1
-    }
-    }while(attemptState<4&&correctState===false)
-    if(correctState===true)
-    {
-        alert("Good guess!")
-    correctAnswers+=1
-    }
-    else
-    {
-    alert("Hey, you'll get it next time.")
-    }
-    var triesState=attemptState+1
-    console.log("The user finished with the question "+correctState+" after "+attempt+" tries.")
-    alert("You got "+correctAnswers+"/7 correct, "+username)
+//Functions end
+questionsYesNo();
+question6();
+question7();
+alert('You got ' + correctAnswers + '/7 correct, ' + username);
